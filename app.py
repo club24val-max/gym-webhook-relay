85c45810f8450dae9b1eb5a012f578e3566d8c69
club24val-max <club24val@gmail.com>
2026-05-30 10:32:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,31 @@ import logging
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-REPLIFY_API_KEY = "KZ3RAX9xJmzmLZJGMcOt4zDx94rHQd89fnlLTFEj"
+REPLIFY_API_KEY_OLD = "KZ3RAX9xJmzmLZJGMcOt4zDx94rHQd89fnlLTFEj"
+REPLIFY_API_KEY_NEW = "D0SNMBdfhl7XbBdu4jUdk8TB85AxDj4r6YZ77lVL"
 BASE_URL_OLD = "https://api.heylibby.com/api/v1/campaigns/{campaign_id}/contacts"
+BASE_URL_NEW = "https://campaign.mylibby.ai/campaigns/{campaign_id}/contacts"
 
 GYMS = {
-    # --- Week Trial ---
-    "wallingford":  "1b9ec7a0-6690-445d-a58c-5b66b899dea4",
-    "torrington":   "927432a1-4f29-4344-968c-e7740a0d93ca",
-    "ridgefield":   "9be3a3b9-661a-4a73-8926-ba4674e1810b",
-    "newtown":      "a5ed599d-ac85-4828-a2b6-44b596cbce97",
-    "newmilford":   "9389e06e-f211-4b89-9f66-592f21101b88",
-    "middletown":   "3529143b-5049-4256-a7f8-ef2b09466c01",
-    "brookfield":   "a89f3263-0892-4704-b3f2-6cafd770725b",
+    # --- Week Trial (new API) ---
+    "wallingford":  {"campaign_id": "1b9ec7a0-6690-445d-a58c-5b66b899dea4", "new": True},
+    "torrington":   {"campaign_id": "927432a1-4f29-4344-968c-e7740a0d93ca", "new": True},
+    "ridgefield":   {"campaign_id": "9be3a3b9-661a-4a73-8926-ba4674e1810b", "new": True},
+    "newtown":      {"campaign_id": "a5ed599d-ac85-4828-a2b6-44b596cbce97", "new": True},
+    "newmilford":   {"campaign_id": "9389e06e-f211-4b89-9f66-592f21101b88", "new": True},
+    "middletown":   {"campaign_id": "3529143b-5049-4256-a7f8-ef2b09466c01", "new": True},
+    "brookfield":   {"campaign_id": "a89f3263-0892-4704-b3f2-6cafd770725b", "new": True},
 
-    # --- Toured / No Join ---
-    "wallingford-toured":  "6f5669f0-b8b6-43d9-aa9b-17fa3119ba8b",
-    "torrington-toured":   "c93d8b19-1187-416c-b0b3-ca92a49a2138",
-    "ridgefield-toured":   "9509535e-b23c-4d85-b4b7-ffac07a54328",
-    "newtown-toured":      "472c0a12-a3c2-4135-b10f-1d0b81e39d5d",
-    "newmilford-toured":   "a6b5f3bb-2a52-42b3-9558-6e99a7267715",
-    "middletown-toured":   "c47c28c2-ad85-4646-bf16-966908b94680",
-    "brookfield-toured":   "ffc21806-fcb9-4355-b4ff-034b8e1b2b22",
+    # --- Toured / No Join (new API) ---
+    "wallingford-toured":  {"campaign_id": "6f5669f0-b8b6-43d9-aa9b-17fa3119ba8b", "new": True},
+    "torrington-toured":   {"campaign_id": "c93d8b19-1187-416c-b0b3-ca92a49a2138", "new": True},
+    "ridgefield-toured":   {"campaign_id": "9509535e-b23c-4d85-b4b7-ffac07a54328", "new": True},
+    "newtown-toured":      {"campaign_id": "472c0a12-a3c2-4135-b10f-1d0b81e39d5d", "new": True},
+    "newmilford-toured":   {"campaign_id": "a6b5f3bb-2a52-42b3-9558-6e99a7267715", "new": True},
+    "middletown-toured":   {"campaign_id": "c47c28c2-ad85-4646-bf16-966908b94680", "new": True},
+    "brookfield-toured":   {"campaign_id": "ffc21806-fcb9-4355-b4ff-034b8e1b2b22", "new": True},
 
-    # --- Past Due 0-30 ---
+    # --- Past Due 0-30 (old API) ---
     "wallingford-pastdue0-30":  "cdc2306a-9f8c-4da0-85fd-62d890740137",
     "torrington-pastdue0-30":   "90f8c7c3-5a48-4499-a2f5-1e1b7383f592",
     "ridgefield-pastdue0-30":   "3cbfe5e4-8521-447c-a7e3-795b10aa8467",
@@ -36,7 +38,7 @@ GYMS = {
     "middletown-pastdue0-30":   "97391e22-f568-473f-a234-3f4c978f0e25",
     "brookfield-pastdue0-30":   "3fddf132-90fb-4b1c-b287-19d6468478bb",
 
-    # --- Past Due 31-60 ---
+    # --- Past Due 31-60 (old API) ---
     "wallingford-pastdue31-60": "bead3dd7-4bad-4a8d-8862-7a057acf069a",
     "torrington-pastdue31-60":  "a85aa596-0c39-4d45-b119-a9ce09a26460",
     "ridgefield-pastdue31-60":  "8b581d95-4287-4cfc-a4cf-7e533c24dc22",
@@ -45,7 +47,7 @@ GYMS = {
     "middletown-pastdue31-60":  "447b3a5a-0dfd-42ff-a5b4-164ab820e0d5",
     "brookfield-pastdue31-60":  "63aa4d47-7428-48f9-bff2-8124b16686fc",
 
-    # --- Past Due 61-90 ---
+    # --- Past Due 61-90 (old API) ---
     "wallingford-pastdue61-90": "128f0096-de10-43a5-82d0-f92f668bacd0",
     "torrington-pastdue61-90":  "216579c4-de5d-49dd-b105-83293b3c3e18",
     "ridgefield-pastdue61-90":  "4f968f09-a704-4245-9223-653804e14e9c",
@@ -54,7 +56,7 @@ GYMS = {
     "middletown-pastdue61-90":  "fadc497d-5375-434e-8c4d-a2c037ada327",
     "brookfield-pastdue61-90":  "6a5dfefc-94cd-42ca-9b82-5bdfb8bc52d1",
 
-    # --- PT Past Due 0-30 ---
+    # --- PT Past Due 0-30 (old API) ---
     "wallingford-ptpastdue0-30":  "40b4abcd-f2fc-4487-9d28-3e98bc6acfd2",
     "torrington-ptpastdue0-30":   "c8820391-9be2-40ee-bb12-7fe974a0d89d",
     "ridgefield-ptpastdue0-30":   "10a65701-cc63-4bc3-bfd8-6f335c5ee531",
@@ -63,7 +65,7 @@ GYMS = {
     "middletown-ptpastdue0-30":   "538923b0-acaa-41d7-8cd2-86fde68e6769",
     "brookfield-ptpastdue0-30":   "55565835-d297-4c5b-b4f5-d9ca51245fa9",
 
-    # --- PT Past Due 31-60 ---
+    # --- PT Past Due 31-60 (old API) ---
     "wallingford-ptpastdue31-60": "6b5200f0-cf2a-4353-868e-2083e1fc3955",
     "torrington-ptpastdue31-60":  "2c9c3fc2-e575-4a1c-b788-4af5a87aa656",
     "ridgefield-ptpastdue31-60":  "337f4b83-1836-49db-9b0f-dee5fbd78c01",
@@ -72,7 +74,7 @@ GYMS = {
     "middletown-ptpastdue31-60":  "efbd172e-0087-4ea9-9e78-09f3b8b0682d",
     "brookfield-ptpastdue31-60":  "367cc250-e51c-4c6a-ab50-11a5bfe64977",
 
-    # --- PT Past Due 61-90 ---
+    # --- PT Past Due 61-90 (old API) ---
     "wallingford-ptpastdue61-90": "7b4264e6-e409-4210-9708-b3e7dc27d485",
     "torrington-ptpastdue61-90":  "9b6a1dc2-570a-45c0-8cc9-8e3dbe4cf9b7",
     "ridgefield-ptpastdue61-90":  "b5b6abae-95e8-4cf6-97b4-9defc2840274",
@@ -81,7 +83,7 @@ GYMS = {
     "middletown-ptpastdue61-90":  "745714c3-76d2-4cf5-806d-8661d95f4a49",
     "brookfield-ptpastdue61-90":  "02ac7b55-4430-4062-91d1-b43d6bd8c3ef",
 
-    # --- Cancelled Member Month 1 ---
+    # --- Cancelled Member Month 1 (old API) ---
     "wallingford-cancelled-month1":  "8da653cb-d4ca-4fbf-a24d-8ef5b765647e",
     "torrington-cancelled-month1":   "",
     "ridgefield-cancelled-month1":   "",
@@ -90,7 +92,7 @@ GYMS = {
     "middletown-cancelled-month1":   "",
     "brookfield-cancelled-month1":   "",
 
-    # --- Cancelled Member Month 2 ---
+    # --- Cancelled Member Month 2 (old API) ---
     "wallingford-cancelled-month2":  "56e7f372-f7a5-4881-a89d-e55566202e3f",
     "torrington-cancelled-month2":   "",
     "ridgefield-cancelled-month2":   "",
@@ -99,7 +101,7 @@ GYMS = {
     "middletown-cancelled-month2":   "",
     "brookfield-cancelled-month2":   "",
 
-    # --- Cancelled Member Month 3 ---
+    # --- Cancelled Member Month 3 (old API) ---
     "wallingford-cancelled-month3":  "bfead2ac-15fe-4b8c-b189-bca4eec5e2a4",
     "torrington-cancelled-month3":   "",
     "ridgefield-cancelled-month3":   "",
@@ -147,15 +149,19 @@ def forward_to_replify(gym_entry, data, gym_name):
         ]
     }
 
+    # Determine URL and API key based on new vs old
+    if isinstance(gym_entry, dict) and gym_entry.get("new"):
+        url = BASE_URL_NEW.format(campaign_id=gym_entry["campaign_id"])
+        api_key = REPLIFY_API_KEY_NEW
+    else:
+        campaign_id = gym_entry if isinstance(gym_entry, str) else gym_entry["campaign_id"]
+        url = BASE_URL_OLD.format(campaign_id=campaign_id)
+        api_key = REPLIFY_API_KEY_OLD
+
     headers = {
         "Content-Type": "application/json",
-        "x-api-key": REPLIFY_API_KEY
+        "x-api-key": api_key
     }
-
-    if isinstance(gym_entry, dict):
-        url = gym_entry["url"]
-    else:
-        url = BASE_URL_OLD.format(campaign_id=gym_entry)
 
     response = requests.post(url, json=payload, headers=headers)
     logging.info(f"[{gym_name}] Replify response: {response.status_code} - {response.text}")
